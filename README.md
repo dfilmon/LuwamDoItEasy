bc9d157684c42b127792079b4b0ecee0b694efd2
dfilmon <abraha.filmon@gmail.com>
2022-12-09 21:33:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LuwamDoItEasy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-4zt3rv)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Form from './Form';
+import 'tailwindcss/tailwind.css';
+
+export default function App() {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+}
